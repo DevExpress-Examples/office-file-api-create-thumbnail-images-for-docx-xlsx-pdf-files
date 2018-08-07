126e581c5bd82641daf2c1bbfc0cd6784344b681
DevExpressExampleBot <fakeEmail>
2018-08-07 16:25:45 +0300

    Source auto update [skip ci]

---
+++ VB/FileInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Linq
+
+Namespace FilesPreviewGenerator
+    Public Class FileInfo
+        Public Sub New(ByVal path As String, ByVal name As String, ByVal documentFormat As String)
+            Me.Path = path
+            Me.Name = name
+            Me.DocumentFormat = documentFormat
+        End Sub
+        Public Property Path() As String
+        Public Property Name() As String
+        Public Property DocumentFormat() As String
+    End Class
+End Namespace
---
+++ VB/ImageExporterHelper.vb
@@ -0,0 +1,52 @@
+﻿Imports DevExpress.Pdf
+Imports DevExpress.Spreadsheet
+Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraRichEdit
+Imports System
+Imports System.Drawing
+Imports System.IO
+Imports System.Linq
+
+Namespace FilesPreviewGenerator
+    Public NotInheritable Class ImageExporterHelper
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function GenerateImageFromExcel(ByVal fileName As String) As Bitmap
+            Using excelDocumentAPI As New Workbook()
+                excelDocumentAPI.LoadDocument(fileName)
+                Return ExportToImage(excelDocumentAPI)
+            End Using
+        End Function
+
+        Public Shared Function GenerateImageFromPDF(ByVal fileName As String, ByVal largestEdgeLength As Integer) As Bitmap
+            Using pdfDocumentAPI As New PdfDocumentProcessor()
+                pdfDocumentAPI.LoadDocument(fileName)
+                Return pdfDocumentAPI.CreateBitmap(1, largestEdgeLength)
+            End Using
+        End Function
+        Public Shared Function GenerateImageFromWord(ByVal fileName As String) As Bitmap
+            Using wordDocumentAPI As New RichEditDocumentServer()
+                wordDocumentAPI.LoadDocument(fileName)
+                Return ExportToImage(wordDocumentAPI)
+            End Using
+        End Function
+        Private Shared Function ExportToImage(ByVal component As IPrintable) As Bitmap
+            Dim pLink As New PrintableComponentLink(New PrintingSystem())
+            pLink.Component = component
+            pLink.CreateDocument(True)
+
+            Dim mStream As New MemoryStream()
+            Dim options As New ImageExportOptions()
+            options.ExportMode = ImageExportMode.SingleFilePageByPage
+            options.PageRange = "1"
+            pLink.ExportToImage(mStream, options)
+            mStream.Position = 0
+            Dim bm As New Bitmap(mStream)
+            mStream.Close()
+
+            Return bm
+        End Function
+    End Class
+End Namespace
